4023a00dbb51b6fe8152472673233086db80256b
iAffected <1104591721@qq.com>
2022-09-27 22:08:38 +0800

    wang_9.27-22:08_删除部分多余文件

---
+++ train.py
@@ -0,0 +1,11 @@
+import torch
+import torchvision.transforms as transforms
+import torch.optim as optim
+import torchvision.transforms.functional as F
+from tqdm import tqdm
+from torch.utils.data import DataLoader
+# from model import ResNet18
+
+
+
+
