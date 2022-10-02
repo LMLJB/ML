188f582e1c9cbf6d92bc1078a0bd5ab2adccf609
iAffected <1104591721@qq.com>
2022-10-02 11:52:08 +0800

    wang_10.2-11.51_临时测试

--- function/Train.py
+++ function/Train.py
@@ -14,9 +14,9 @@ from Model import resnet18, resnet50  # resnet18/resnet50
 # 超参数
 LR = 0.00001      # 学习率
 DEVICE = "cuda:0" if torch.cuda.is_available() else "cpu"  # 运行模型选择的设备
-BATCH_SIZE = 200  # 一次输入训练的批量
+BATCH_SIZE = 128  # 一次输入训练的批量
 EPOCH = 1         # 训练次数
-NUM_WORKERS = 0
+NUM_WORKERS = 2
 LOSS_FUNC = nn.CrossEntropyLoss()  # 定义损失函数
 SEED = 1          # 固定种子，以保证获取相同的训练结果
 MODEL = resnet18()
@@ -42,8 +42,6 @@ def train_model():
     log_all_epoch_history = []
     # 开始训练
     for epoch in range(EPOCH):
-        # 单个batch训练
-        running_loss = 0.0
         log_train_loss = []  # 记录训练时每个batch的损失值
         loop = tqdm(train_loader)
         for inputs, labels in loop:  # 分配batch data -> inputs为输入图片, labels为输入图片的类型（标签）
@@ -52,13 +50,11 @@ def train_model():
             output = model(inputs)            # 将数据放入resnet中计算输出
             loss = loss_func(output, labels)
             optimizer.zero_grad()             # 清空过往梯度
-            loss.backward()                   # 反向传播，计算当前梯度；
+            loss.backward()                   # 反向传播，计算当前梯度
             optimizer.step()                  # 根据梯度更新网络参数
             loss = loss.to(DEVICE)            # 默认使用gpu计算损失
-            running_loss += loss.item()
-            log_train_loss.append(running_loss)
-            log_all_epoch_history.append(running_loss)
-            running_loss = 0.0
+            log_train_loss.append(loss)
+            log_all_epoch_history.append(loss)
         # show_train_loss(log_train_loss)  # 显示单个epoch训练的loss变化过程
     history_data = {"batch_size": BATCH_SIZE, "epoc": EPOCH, "train_loss_change": log_all_epoch_history}
     save_data(train_history_path, history_data)  # 将训练数据保存到文件中
@@ -66,4 +62,5 @@ def train_model():
     show_train_loss(log_all_epoch_history)  # 显示所有loss变化过程
 
 
-train_model()
+if __name__ == '__main__':
+    train_model()
